4c4b1ce32e379f3bd1ccd6029878cf595f7983a4
arslan <arslant374@gmail.com>
2022-06-26 20:38:28 +0500

    perm

--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.permissions import BasePermission, IsAdminUser
+from rest_framework.decorators import action, permission_classes
+from rest_framework.permissions import BasePermission, IsAdminUser,IsAuthenticated
 
 from .serializers import UserSerializer,UserTierSerializer
 from. models import CustomUser as User,UserTier
@@ -56,7 +56,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-
+    @permission_classes([IsAuthenticated])
     @action(detail=True, methods=['put'])
     def change_password(self,request, pk = None):
 
